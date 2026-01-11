0dc1ad77c8d10c76cfab8822615666ee22a0bb9a
Eddie Feng <eddiefeng05@gmail.com>
2026-01-11 17:29:06 +0800

    v0.3.0 finish ch4

--- build/my_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eddie/Documents/ROS2_moveit2_learning/ros2_ws/src/my_robot_description
-# Build directory: /home/eddie/Documents/ROS2_moveit2_learning/build/my_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
