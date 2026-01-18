4edbd2fd2651c289cd3198b61f5672945cb88cc1
Eddie Feng <eddiefeng05@gmail.com>
2026-01-18 10:16:17 +0800

    Ch7-2 Make the Arm Move with the MoveIt C++ API

--- docs/README-ch7.md
+++ docs/README-ch7.md
@@ -26,4 +26,44 @@ sudo apt install ros-jazzy-ros2-control ros-jazzy-ros2-controllers
 
 ### Ch7-1 Intro
 
+### Ch7-2 Make the Arm Move with the MoveIt C++ API
 
+1. Create new package named my_robot_command_cpp
+
+```
+ros2 pkg create my_robot_command_cpp --build-type ament_cmake --dependencies rclcpp
+
+```
+
+2. New cpp file in command_cpp/src , and wirte the ros2 sturcture.
+
+```
+ros2_ws/src/my_robot_command_cpp/src/test_moveit.cpp
+```
+
+3. New about moveit code in test_moveit.cpp
+
+4. Add new code in CMakeList.txt
+
+ros2_ws/src/my_robot_command_cpp/CMakeLists.txt
+
+5. Add new code in packages.xml
+
+ros2_ws/src/my_robot_command_cpp/package.xml
+
+6. Build and Launch code
+
+* Terminal 1
+```
+colcon build --packages-select my_robot_command_cpp 
+
+ros2 launch my_robot_bringup my_robot.launch.xml 
+
+```
+* Terminal 2
+```
+ros2 run my_robot_command_cpp test_moveit 
+
+```
+
+7. Extention: Add new arm pose(home) and add gripper group. 
---
+++ ros2_ws/src/my_robot_command_cpp/src/test_moveit.cpp
@@ -0,0 +1,44 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<rclcpp::Node>("test_moveit");
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+    auto spinner = std::thread([&executor]() { executor.spin(); });
+
+    auto arm = moveit::planning_interface::MoveGroupInterface(node, "arm");
+    arm.setMaxVelocityScalingFactor(0.1); // 10% of max velocity
+    arm.setMaxAccelerationScalingFactor(0.1); // 10% of max acceleration
+
+    auto gripper = moveit::planning_interface::MoveGroupInterface(node, "gripper");
+
+    // Named goal
+
+    arm.setStartStateToCurrentState();
+    arm.setNamedTarget("pose_1");
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan1;
+    bool success1 = (arm.plan(plan1) == moveit::core::MoveItErrorCode::SUCCESS);
+
+    if(success1) {
+        arm.execute(plan1);
+    }
+
+    arm.setStartStateToCurrentState();
+    arm.setNamedTarget("home");
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan2;
+    bool success2 = (arm.plan(plan2) == moveit::core::MoveItErrorCode::SUCCESS);
+
+    if(success2) {
+        arm.execute(plan2);
+    }
+
+    rclcpp::shutdown();
+    spinner.join();
+    return 0;
+}
